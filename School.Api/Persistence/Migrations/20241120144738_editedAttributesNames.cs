18e37e9d4a27d94c9b72656c8a269098402e2482
Salah0123 <hossameldinsalah67@gmail.com>
2024-11-20 21:29:10 +0300

    JKL

---
+++ School.Api/Persistence/Migrations/20241120144738_editedAttributesNames.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class editedAttributesNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Count",
+                table: "Regions",
+                newName: "TeacherCount");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TeacherCount",
+                table: "Regions",
+                newName: "Count");
+        }
+    }
+}
